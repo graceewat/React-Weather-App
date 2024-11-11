bfa3621d45a02d77a36fc941755e5f774a846ce2
graceewat <160866251+graceewat@users.noreply.github.com>
2024-11-11 16:01:00 +0000

    Update to search.js and index.js to communicate with each other

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./styles.css";
-import Search from "./Search";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <Search />
     </div>
   );
 }
